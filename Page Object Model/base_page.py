a2d76540db6833c971ef32e27268a25f2e21d289
Anna Vikulova <annie.vikulova@mail.ru>
2025-05-04 22:01:44 +0300

    Commit 1

---
+++ Page Object Model/base_page.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click(self, *locator):
+        element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator))
+        element.click()
+
+    def get_text(self, *locator):
+        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+        return element.text
--- tests/test_order.py
+++ tests/test_order.py
@@ -1,21 +1,28 @@
 import pytest
+import allure
 from selenium import webdriver
 from page_objects.main_page import MainPage
 from page_objects.order_page import OrderPage
+from data.test_data import ORDER_DATA
+from urls.urls import SCOOTER_URL
 
-@pytest.mark.parametrize("name, phone", [
-    ("Иван Иванов", "+79012345678"),
-    ("Петр Петров", "+79098765432"),
-])
-def test_order_creation(name, phone):
-    driver = webdriver.Chrome()
-    driver.get("https://qa-scooter.praktikum-services.ru/")
+@pytest.mark.parametrize("name, phone", ORDER_DATA)
+@allure.step("Проверка создания заказа с именем: {name} и телефоном: {phone}")
+def test_order_creation(driver, name, phone):
+    # Проверяем положительный сценарий для обеих кнопок
     main_page = MainPage(driver)
     order_page = OrderPage(driver)
 
+    # Первый сценарий: нажать кнопку "Заказать" в верхней части страницы
+    driver.get(SCOOTER_URL)
     main_page.click_order_button()
     order_page.fill_order_form(name, phone)
     order_page.submit_order()
-
     assert order_page.is_success_message_displayed()
-    driver.quit()
+
+    # Второй сценарий: нажать кнопку "Заказать" в нижней части страницы
+    driver.get(SCOOTER_URL)
+    main_page.click_order_button()
+    order_page.fill_order_form(name, phone)
+    order_page.submit_order()
+    assert order_page.is_success_message_displayed()
